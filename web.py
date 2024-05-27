92528abc5eb5b783b2463dc3864b40f1c17d697b
zaid <165736988+copitotactico@users.noreply.github.com>
2024-05-27 17:23:15 -0600

    Create web.py
    a

---
+++ web.py
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"></link>
+    <title>Document</title>
+
+</head>
+<body>
+    <h1>hola</h1>
+    <h3>arthur morgan</h3>
+    <p>es el mejor <br>juego del mundo</p>
+    
+    <img src="https://www.ixpap.com/images/2021/08/RDR2-Wallpaper-4.jpg"></img>
+    
+    <ul>
+        <li>lista</li>
+        <li>lista 2</li>
+    </ul>
+    <input type="text">
+    <input type="password">
+</body>
+</html>
